9a8e2a3077689db0d00c2db24750671e8fc73fb8
me-cooper <github@makesmart.net>
2023-04-06 21:14:31 +0200

    added bootstrap support out of the box

--- settings.js
+++ settings.js
@@ -3,12 +3,10 @@ module.exports = {
         port: 3000
     },
     auth: {
-        
         secret: 'mySuperSecretServerwideTokenHere',
 
         // How long should the login session be valid?
-                                     // Minutes      // Hours        // Days
-        valid:  (1000 * 60)         * 5              * 24            * 14 
-        
+                                    // Minutes      // Hours        // Days
+        valid:  (1000 * 60)         * 60              * 24            * 14 
     }
 }
